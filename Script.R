4c2834101d91a719a35ad005262810027fc3ef24
Emma Sheard <emmajanesheard@hotmail.co.uk>
2021-07-07 17:12:58 +0100

    Load libraries and load data

---
+++ Script.R
@@ -0,0 +1,9 @@
+library(dplyr) # load libraries
+library(ggplot2)
+
+data("mtcars") # load data
+
+head(mtcars) # view/check data is as expected
+glimpse(mtcars)
+
+# Commit - 'Load libraries and load data'
