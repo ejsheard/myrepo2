a4aa56f8c16ef76fbfb6f69adc1b0c4ee3c748f2
Emma Sheard <emmajanesheard@hotmail.co.uk>
2021-07-07 17:15:39 +0100

    summarize disp and hp by cyl

--- Script.R
+++ Script.R
@@ -7,3 +7,12 @@ head(mtcars) # view/check data is as expected
 glimpse(mtcars)
 
 # Commit - 'Load libraries and load data'
+
+mtcars_summary <- mtcars %>%
+  group_by(cyl) %>%  # group by cylinder type
+  summarise(mean_displ = mean(disp), # show mean of disp per group
+            mean_hp = mean(hp)) #  show mean of hp per group
+
+mtcars_summary # view summary created
+
+# Commit - 'summarize disp and hp by cyl'
